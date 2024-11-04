0b26b888daffb899767a614cd80bb9ef4230afbe
박성완 <swany0509@naver.com>
2024-11-04 20:11:44 +0900

    add 기본 파일트리 생성

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.jsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
+
+//https://velog.io/@jungsw586/React-%EA%B0%9C%EB%B0%9C%ED%99%98%EA%B2%BD-%EC%84%B8%ED%8C%85%ED%95%98%EA%B8%B0-2.-React-Router-%EC%84%A4%EC%B9%98
