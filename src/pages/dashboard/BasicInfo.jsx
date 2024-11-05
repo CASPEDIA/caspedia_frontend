a5bf16e3ad83df631d7b86094ad963cbeefd8798
박성완 <swany0509@naver.com>
2024-11-05 23:40:23 +0900

    add index page 작성

--- src/pages/dashboard/BasicInfo.jsx
+++ src/pages/dashboard/BasicInfo.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import './BasicInfo.css'
 
 export default function BasicInfo() {
   return (
-    <div>BasicInfo</div>
+    <div>
+      <div className='m-5'>
+        <img src="/img/index_main.png" alt="caspedia" style={{ width: '70%' }}/>
+      </div>
+      <div className='mx-4 p-4 index_introduction'>
+        <br />
+        CAST가 함께 만들어가는 새로운 우주<br />
+        CAST OB 제작 웹사이트 CASPEDIA입니다. <br />
+        우리 함께 만들어가요. <br />
+        많은 평가 부탁드립니다. <br />
+        <br />
+        이용 중 불편하신 점이 있다면 관리자에게 문의 바랍니다. <br />
+        <br />
+        관리자: 박성완, 박하민, 윤병민, 김고은, 석명진 <br />
+        <br /><br />
+        *신규* 2024/10/10 업데이트 <br />
+        쓸데없는 기능 1을 추가하였습니다. <br />
+        <br />
+        2024/08/10 업데이트 <br />
+        병민이가 햄버거를 수정하였습니다. <br />
+        <br />
+        2024/07/10 업데이트 <br />
+        병민이는 아주 행복하고 싶습니다. <br /><br />
+      </div>
+    </div>
   )
 }
