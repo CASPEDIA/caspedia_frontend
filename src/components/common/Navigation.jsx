cbf089e9868dd9f5ce7b14a5e36139e713e34058
박성완 <swany0509@naver.com>
2024-11-05 21:46:32 +0900

    add router 설정 완료

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import "./Navigation.css"
 import { Col, Row } from 'react-bootstrap'
-import { useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 export default function Navigation() {
-  const history = useHistory();
+
   return (
     <div className='navigation p-2'>
       <Row className='mt-2'>
-        <Col onClick={history.push('/')}>
-          <img src="/img/toDashboard.svg" alt="Home" className="nav-icon" />
+        <Col>
+          <Link to="/">
+            <img src="/img/toDashboard.svg" alt="Home" className="nav-icon" />
+          </Link>
         </Col>
         <Col className='flex-grow-1'/>
-        <Col onClick={history.push('/user/3')}>
-          <img src="/img/toUser.svg" alt="My Page" className="nav-icon"/>
+        <Col>
+          <Link to="/user/3">
+            <img src="/img/toUser.svg" alt="My Page" className="nav-icon" />
+          </Link>
         </Col>
       </Row>
     </div>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import React  from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import BoardGameDetail from "./boardgame/BoardGameDetail";
 import BasicInfo from "./dashboard/BasicInfo";
 import NotFound from "./NotFound";
@@ -13,32 +13,41 @@ export default function Main() {
 
   return (
     <>
+      <Routes>
+        <Route path="signin" element={<SignIn />}/>
+        <Route exact path="/">
+          <Route exact index element={<BasicInfo />}/>
+          <Route path="user/:nanoid" element={<UserDetail />}/>
+          <Route path="search" element={<SearchResult />}/>
+          <Route path="boardgame/:boardid" element={<BoardGameDetail />}/>
+          <Route path="rating" element={<Rating />}/>
+        </Route>
+        <Route path="*" element={<NotFound />}/>
+      </Routes>
       {/* <Navbar /> */}
-        <Router>
-          <Switch>
-            <Route path="/signin">
-              <SignIn />
-            </Route>
-            <Route exact path="/">
-              <BasicInfo />
-            </Route>
-            <Route path="/user/:nanoid" >
-              <UserDetail />
-            </Route>
-            <Route path="/search">
-              <SearchResult />
-            </Route>
-            <Route path="/boardgame/:boardid">
-              <BoardGameDetail />
-            </Route>
-            <Route path="/rating">
-              <Rating />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </Router>
+        {/* <Routes>
+          <Route path="/signin">
+            <SignIn />
+          </Route>
+          <Route exact path="/">
+            <BasicInfo />
+          </Route>
+          <Route path="/user/:nanoid" >
+            <UserDetail />
+          </Route>
+          <Route path="/search">
+            <SearchResult />
+          </Route>
+          <Route path="/boardgame/:boardid">
+            <BoardGameDetail />
+          </Route>
+          <Route path="/rating">
+            <Rating />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Routes> */}
       {/* <Navigation /> */}
     </>
   );
