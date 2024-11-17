506c4d0b0d3045a087e8fbc4fa670212498f5bee
박성완 <swany0509@naver.com>
2024-11-17 23:54:32 +0900

    add 커스텀 카드 변경

--- src/components/common/CustomCard.jsx
+++ src/components/common/CustomCard.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./CustomCard.css"
 
-const CommonBox = ({ height, children }) => {
+const CustomCard = ({ 
+  height, 
+  children, 
+  padding = "5%",
+  justifyContent = "center",
+}) => {
   const style = {
     height: height, // 동적으로 전달된 높이
+    padding: padding,
+    justifyContent: justifyContent
   };
 
   return <div style={style} className="custom-card">{children}</div>;
 };
 
-CommonBox.propTypes = {
+CustomCard.propTypes = {
   height: PropTypes.string.isRequired, // 높이를 필수로 받음
   children: PropTypes.node, // 자식 요소를 받음
 };
 
-export default CommonBox;
+export default CustomCard;
