b45f34df7d51f82f1b7843df6f4b9b4b21fd4e4e
박성완 <swany0509@naver.com>
2024-11-19 23:06:15 +0900

    add 유저 페이지 정보

--- src/components/user/UserRating.jsx
+++ src/components/user/UserRating.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './UserRating.css'
+import { useNavigate } from 'react-router-dom'
+import CancelButton from '../common/CancelButton';
 
-export default function UserRating() {
+export default function UserRating({
+  nanoid="guest",
+  rating_key=10,
+  boardgame_key=66690,
+  image_url="https://cf.geekdo-images.com/fEawLvevkxPv9AQ3mSiwVQ__itemrep/img/6UJpoKwtjxUm965dI017XMrgGDE=/fit-in/246x300/filters:strip_icc()/pic1747320.jpg",
+  comment="좋았어요!!",
+  score=10,
+  name_eng="Dominion : Prosperity",
+  name_kor="도미니언 : 약속된 번영",
+  created="2024-11-16T15:47:37.450685",
+  updated="2024-11-16T19:24:48.835425",
+  tag_key="111110000000000000000000"
+}) {
+  const navigate = useNavigate();
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpand = () => {
+    setIsExpanded((prev) => !prev);
+  };
   return (
-    <div>UserRating</div>
+    <div className='div-user-rating-card'>
+      <div className='div-user-rating-basic-info'>
+        <div className='div-user-rating'>
+          <img src={image_url} width="15%" alt="이미지" onClick={() => navigate("/boardgame/" + boardgame_key)}/>
+          <div style={{"textAlign":"left", "padding":"1rem"}}>
+            <strong onClick={() => navigate("/boardgame/" + boardgame_key)}>{name_kor}</strong>
+          </div>
+        </div>
+        <div className='rating-circle'>
+          {score}
+        </div>
+      </div>
+      {isExpanded ?
+        <>
+          <div className='div-tag-container'>
+            <SelectedTag text="3인 베스트🤟"/>
+            <SelectedTag text="숙련자들이 즐기는👨🏻‍🎓"/>
+            <SelectedTag text="또 해보고 싶은💘"/>
+          </div>
+          <p style={{"textAlign": "left", "padding" : "0% 3% 1% 3%"}}>
+            {comment}
+          </p>
+          <div className='div-collapse-rating'>
+            <CancelButton 
+              text="접기"
+              onClick={toggleExpand}
+            />
+          </div>
+        </>
+        :
+        <>
+          <p className='p-rating-comment' onClick={toggleExpand}>
+            {comment}
+          </p>
+        </>  
+      }
+    </div>
   )
+}
+
+function SelectedTag({
+  text="Selected"
+}) {
+  return(
+    <div className='div-tag-item'>
+      {text}
+    </div>
+  );
 }
--- src/pages/user/UserDetail.jsx
+++ src/pages/user/UserDetail.jsx
@@ -1,7 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserRating from '../../components/user/UserRating'
+import './UserDetail.css'
+import CustomButton from '../../components/common/CustomButton';
 
 export default function UserDetail() {
+  const [introduce, setIntroduce] = useState("");
+  const introduceChange = (e) => {
+    setIntroduce(e.target.value);
+  }
+  const logout = () => {
+    console.log("로그아웃");
+  }
   return (
-    <div>UserDetail</div>
+    <>
+      <div className='div-user-basic-info'>
+        <div className='div-user-name-info'>
+          <img src="/user_profile/profile_1.png" width="23%" alt="이미지" />
+          <div className='div-user-inner-info'>
+            <h1 className='h1-nickname'>
+              성와니&nbsp;
+              <img src="/img/F1_edit_pencil.png" width="5%" alt="" />
+            </h1>
+            <div className='div-name-info'>
+              <h3>박성완</h3>
+              <CustomButton text="로그아웃" onClick={logout} />
+            </div>
+          </div>
+        </div>
+        <textarea 
+          value={introduce}
+          onChange={introduceChange}
+          rows="5"
+          placeholder="자기소개를 입력하세요."
+          className='custom-textarea'
+        />
+        <div className='div-calculated-infos'>
+          <div className='div-outer-card' style={{"height" : "17rem", "width" : "49%"}}>
+            <div className='div-inner-card mr-2'>
+              <h1><strong>성와니 님이<br />좋아요한 게임들</strong></h1>
+              <div style={{"height" : "1rem"}}></div>
+              <h1 style={{"fontSize" : "5.5rem"}}><strong>135</strong></h1>
+            </div>  
+          </div>
+          <div style={{"width" : "49%"}}>
+            <div className='div-outer-card' style={{"height" : "8rem", "marginBottom" : "1rem"}}>
+              <div className='div-inner-card'>
+                <h3><strong>리뷰</strong></h3>
+                <div style={{"height" : "1rem"}}></div>
+                <h3><strong>26</strong></h3>
+              </div>
+            </div>
+            <div className='div-outer-card' style={{"height" : "8rem"}}>
+              <div className='div-inner-card'>
+                <h3><strong>평균 평점</strong></h3>
+                <div style={{"height" : "1rem"}}></div>
+                <h3><strong>9.3</strong></h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h1 className='h1-review-name'>성와니 님의 리뷰</h1>
+      </div>
+      <div className='div-review-background'>
+        <div className='div-sort-tabs'>
+          <span>최신순</span> / <span>평점순</span>
+        </div>
+        <UserRating />
+        <UserRating />
+        <UserRating />
+        <UserRating />
+        <UserRating />
+        <UserRating />
+        <h3 style={{"color" : "white"}}>리뷰 더보기</h3>
+      </div>
+    </>
   )
 }
