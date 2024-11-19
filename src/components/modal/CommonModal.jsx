162d3dfbb1cf2e88d25aafc5f2a52cf43a2ddea6
박성완 <swany0509@naver.com>
2024-11-19 20:56:16 +0900

    add 모달 설정

---
+++ src/components/modal/CommonModal.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from "prop-types";
+import './CommonModal.css'
+
+const CommonModal = ({
+  isModalOpen,
+  closeModal,
+  children
+}) => {
+  if (!isModalOpen) return null;
+  return (
+    <div className='div-modal-overlay' onClick={closeModal}>
+      <div className='div-modal-container' >
+        <div className='div-modal-inner'>
+          {children}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+CommonModal.propTypes = {
+  // height: PropTypes.string.isRequired, // 높이를 필수로 받음
+  children: PropTypes.node, // 자식 요소를 받음
+};
+
+export default CommonModal;
