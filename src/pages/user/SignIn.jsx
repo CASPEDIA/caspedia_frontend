da4ac9f5ba8ae89ec6d415d99b9b5e76bd4060a1
박성완 <swany0509@naver.com>
2024-11-22 22:19:56 +0900

    add 로그인, 메인화면 반응형

--- src/pages/user/SignIn.jsx
+++ src/pages/user/SignIn.jsx
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Signin.css'
-import CustomButton from '../../components/common/CustomButton'
+import CustomButton from 'components/common/CustomButton'
 
 export default function SignIn() {
+  const idInputRef = useRef(null);
+
+  useEffect(() => {
+    if (idInputRef.current) {
+      idInputRef.current.focus();
+    }
+  })
   return (
     <div className='div-signin'>
-      <img src="/img/F5_main_logo.png" width="80%" alt="메인 로고" />
+      <img src="/img/F5_main_logo.png" style={{height:"30vh", width:"auto"}}  alt="메인 로고" />
+      <div style={{height:"8vh"}}></div>
       <div className='div-signin-form'>
-        <h2>
+        <div>
           아이디
-        </h2>
+        </div>
         <input 
           className='custom-input'
           type="text"
+          ref={idInputRef}
           placeholder='아이디 입력...'
         />
-        <h2>
+        <div>
           비밀번호
-        </h2>
+        </div>
         <input 
           className='custom-input'
           type="password"
@@ -27,9 +36,9 @@ export default function SignIn() {
           text="로그인"
         />
       </div>
-      <h2 className='mt-3 mx-3'>
+      <div className='mt-3 mx-3'>
         비밀번호 분실 및 기타 문의사항은<br /> 관리자에게 연락 바랍니다.
-      </h2>
+      </div>
     </div>
   )
 }
