c46e14c0e00b81e2e6189b468b653e19d260ffcf
박성완 <swany0509@naver.com>
2024-11-26 23:10:29 +0900

    add axios 추가

--- src/api/http.js
+++ src/api/http.js
@@ -1,12 +1,45 @@
-// import axios from "axios";
+import axios from "axios";
 
-// axios.defaults.withCredentials = true;
-// // axios 객체 생성
-// export default axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   // baseURL: 'http://i8c211.p.ssafy.io:5001/',
-//   // baseURL: 'http://localhost:5001/' ,
-//   headers: {
-//     "Content-Type": "application/json;charset=utf-8",
-//   },
-// });
+axios.defaults.withCredentials = true;
+// axios 객체 생성
+const http = axios.create({
+  baseURL: 'http://localhost:8080/api',
+  // baseURL: import.meta.env.APP_API_URL,
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+});
+
+// 요청 인터셉터
+http.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('jwtToken');
+    if( token && !config.url.includes('/login')) {
+      config.headers.Authorization=`Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+http.interceptors.response.use(
+  (response)=> {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      handleUnauthorized();
+    }
+  }
+);
+
+function handleUnauthorized() {
+  localStorage.removeItem('jwtToken'); // JWT 토큰 삭제
+  localStorage.removeItem('nanoid'); // 현재 로그인 유저 nanoid 삭제
+  window.location.href = '/signin'; // 로그인 페이지로 이동
+}
+
+export default http;
