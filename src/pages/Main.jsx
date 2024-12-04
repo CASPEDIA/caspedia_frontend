a3a4c159fb3cc1d6869ac31ff2e135a5bb2b1bf9
박성완 <swany0509@naver.com>
2024-12-04 21:28:52 +0900

    add 로딩 시 최상단으로 이동

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,6 +8,7 @@ import SearchResult from "pages/search/SearchResult";
 import SignIn from "pages/user/SignIn";
 import UserDetail from "pages/user/UserDetail";
 import RequireAuth from 'hooks/RequireAuth';
+import ScrollToTop from './ScrollToTop';
 
 
 export default function Main() {
@@ -19,26 +20,31 @@ export default function Main() {
         <Route exact path="/">
           <Route exact index element={
             <RequireAuth>
+              <ScrollToTop />
               <BasicInfo />
             </RequireAuth>
           }/>
           <Route path="user/:nanoid" element={
             <RequireAuth>
+              <ScrollToTop />
               <UserDetail />
             </RequireAuth>
           }/>
           <Route path="search" element={
             <RequireAuth>
+              <ScrollToTop />
               <SearchResult />
             </RequireAuth>
           }/>
           <Route path="boardgame/:boardid" element={
             <RequireAuth>
+              <ScrollToTop />
               <BoardGameDetail />
             </RequireAuth>
           }/>
           <Route path="rating/:boardid" element={
             <RequireAuth>
+              <ScrollToTop />
               <Rating />
             </RequireAuth>
           }/>
