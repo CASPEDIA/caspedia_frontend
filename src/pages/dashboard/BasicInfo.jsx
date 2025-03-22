33e4e17f140232e1c6547fb74c5c8b71087e2a9b
swany0509 <swany0509@naver.com>
2025-03-22 21:50:17 +0900

    add event image

--- src/pages/dashboard/BasicInfo.jsx
+++ src/pages/dashboard/BasicInfo.jsx
@@ -6,14 +6,11 @@ import RecentUser from './RecentUser'
 export default function BasicInfo() {
   return (
     <div>
-      <div className='m-5'>
-        <img src="/img/F5_main_logo.png" alt="caspedia" style={{ height: "30vh","width": 'auto' }}/>
+      <div className='m-3'>
+        <img src="/img/F5_event_01.png" alt="caspedia" style={{ height: "auto","width": "100%" }}/>
+        {/* <img src="/img/F5_main_logo.png" alt="caspedia" style={{ height: "30vh","width": 'auto' }}/> */}
       </div>
       <div className='div-index_introduction'>
-        CAST가 함께 만들어가는 새로운 우주<br />
-        CAST OB 제작 웹사이트 CASPEDIA입니다. <br />
-        우리 함께 만들어가요. <br />
-        많은 평가 부탁드립니다. <br />
         <br />
         이용 중 불편하신 점이 있다면 관리자에게 문의 바랍니다. <br />
         <br />
